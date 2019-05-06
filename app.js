b5b740670ea5f813e58b6dbf78ae9375c8c27182
anirudh-kac <anirudh.kac@yahoo.com>
2019-05-06 22:05:12 +0530

    added css and some jQuery to the information page

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const bodyParser=require("body-parser");
 const request=require("request");
 const app=express();
 app.use(bodyParser.urlencoded({extended:true}));
+
 app.use(express.static("public"));
+
 const path = require('path');
 const PORT = process.env.PORT || 3000;
 
@@ -41,14 +43,24 @@ app.post("/",function(req,res){
       var humidity=data.main.humidity;
       var windSpeed=data.wind.speed;
 
-      res.write("<p>Beta V2</p>");
-      res.write("<h1>Hey Anirudh, Here's your weather data for "+value+"<h1>");
-      res.write("<img src='http://openweathermap.org/img/w/"+icon+".png'>");
+
+//HTML elements start here
+      res.write("<link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.8.1/css/all.css' integrity='sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf' crossorigin='anonymous'>");
+      res.write("<link href='styles2.css' rel='stylesheet'>");
+      res.write("<h1 class='title' value="+value+">Hey Anirudh, Here's your weather data for "+value+"<h1>");
+      //res.write("<img src='http://openweathermap.org/img/w/"+icon+".png' class='icon'>");
       res.write("<h1>"+description+"<h1>");
-      res.write("<h1>The temperature is "+temp+"<h1>");
+      res.write("<h1 class = 'temperature ' value="+temp+">The temperature is "+temp+"<h1>");
       res.write("<h1>The wind speed is "+windSpeed+" mph <h1>");
       res.write("<h1>The humidity is "+humidity+" % <h1>");
-        res.send();
+      res.write("<h1 class ='last '> ***<h1>");
+      res.write("<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js'></script>");
+      res.write(" <script src='index.js' > </script> ");
+
+// HTML elements end here
+
+
+      res.send();
 
 
     }else{
---
+++ public/index.js
@@ -0,0 +1,7 @@
+var bg =$(".title").attr("value");
+
+if(bg==="Anantnag,IN"){
+  $("body").addClass("kmr");
+}else{
+  $("body").addClass("blr");
+}
